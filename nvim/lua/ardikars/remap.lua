879a094006d2d04c9e3d17c939299fa7ca38709b
Ardika Rommy Sanjaya <contact@ardikars.com>
2024-03-14 15:03:09 +0700

    Force to not use arrow keys

--- nvim/lua/ardikars/remap.lua
+++ nvim/lua/ardikars/remap.lua
@@ -12,3 +12,12 @@ vim.keymap.set('i', '<C-c>', '<Esc>')
 vim.keymap.set('v', '<C-c>', '<Esc>')
 
 vim.keymap.set('n', 'Q', '<nop>')
+
+vim.keymap.set('n', '<Up>', '<Nop>')
+vim.keymap.set('n', '<Down>', '<Nop>')
+vim.keymap.set('n', '<Left>', '<Nop>')
+vim.keymap.set('n', '<Right>', '<Nop>')
+vim.keymap.set('i', '<Up>', '<Nop>')
+vim.keymap.set('i', '<Down>', '<Nop>')
+vim.keymap.set('i', '<Left>', '<Nop>')
+vim.keymap.set('i', '<Right>', '<Nop>')
