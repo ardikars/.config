5d6e17e7ca55f4ef3be9b846bf7247e7dab6ebab
Ardika Rommy Sanjaya <contact@ardikars.com>
2024-03-14 14:31:55 +0700

    add gitignore file to treesitter config

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "rust", "python", "bash", "lua", "vim", "vimdoc", "query", "json", "yaml" },
+  ensure_installed = { "rust", "python", "bash", "lua", "vim", "vimdoc", "query", "json", "yaml", "gitignore" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
