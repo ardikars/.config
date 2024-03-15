dc25d74de498ff1f62a479803d3e4dc8654d1b3c
Ardika Rommy Sanjaya <contact@ardikars.com>
2024-03-15 16:01:26 +0700

    Add lsp

--- nvim/lua/ardikars/packer.lua
+++ nvim/lua/ardikars/packer.lua
@@ -8,4 +8,18 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use { 'nvim-telescope/telescope.nvim', tag = '0.1.6', requires = { { 'nvim-lua/plenary.nvim' } } }
   use ('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+  use {
+		  'VonHeikemen/lsp-zero.nvim',
+		  branch = 'v3.x',
+		  requires = {
+				  {'williamboman/mason.nvim'},
+				  {'williamboman/mason-lspconfig.nvim'},
+				  -- LSP Support
+				  {'neovim/nvim-lspconfig'},
+				  -- Autocompletion
+				  {'hrsh7th/nvim-cmp'},
+				  {'hrsh7th/cmp-nvim-lsp'},
+				  {'L3MON4D3/LuaSnip'},
+		  }
+  }
 end)
