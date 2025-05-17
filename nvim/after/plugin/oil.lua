72071a9b98c87652454aa8444cbda18cb43fad49
Ardika Rommy Sanjaya <contact@ardikars.com>
2025-05-17 19:20:48 +0700

    simplify conf

--- nvim/after/plugin/oil.lua
+++ nvim/after/plugin/oil.lua
@@ -2,17 +2,7 @@ require("oil").setup({
   -- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
   -- Set to false if you want some other plugin (e.g. netrw) to open when you edit directories.
   default_file_explorer = true,
-  -- Keymaps in oil buffer. Can be any value that `vim.keymap.set` accepts OR a table of keymap
-  -- options with a `callback` (e.g. { callback = function() ... end, desc = "", mode = "n" })
-  -- Additionally, if it is a string that matches "actions.<name>",
-  -- it will use the mapping at require("oil.actions").<name>
-  -- Set to `false` to remove a keymap
-  -- See :help oil-actions for a list of all available actions
-  keymaps = {
-    ["<CR>"] = "actions.select",
-    ["<C-p>"] = "actions.preview",
-    ["<C-c>"] = { "actions.close", mode = "n" },
-    ["<C-l>"] = "actions.refresh",
+	keymaps = {
     ["-"] = { "actions.parent", mode = "n" },
   },
   -- Set to false to disable all of the above keymaps
@@ -20,11 +10,6 @@ require("oil").setup({
 	view_options = {
 		show_hidden = true,
 	},
-	preview_win = {
-		disable_preview = function(filename)
-      return false
-    end,
-	}
 })
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
