d7962011089abcf95876c6859a0c16f27d8338ba
Ardika Rommy Sanjaya <contact@ardikars.com>
2025-06-07 07:22:06 +0700

    Add missing <CR>

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -17,6 +17,6 @@ return {
             },
         })
 
-        vim.keymap.set("n", "-", "<CMD>Oil", { desc = "Open parent directory" })
+        vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
     end
 }
